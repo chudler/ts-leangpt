34cc24d68f43e9bbef4bed77650dff7ba91b0b06
Colin Hudler <chudler@uchicago.edu>
2024-01-07 02:11:39 -0600

    use mathlib not core

--- src/division.lean
+++ src/division.lean
@@ -1,4 +1,6 @@
-import init.data.nat.basic
+import data.nat.basic
 
 lemma division (m n : ℕ) (h: m > 0) : ∃!k : ℕ, ∃!r : ℕ, n = m*k + r ∧ r < m :=
+begin
 by admit
+end
--- src/division_theorem.lean
+++ src/division_theorem.lean
@@ -1,4 +1,4 @@
-import init.data.nat.basic
+import data.nat.basic
 
 theorem division_theorem (a b : ℕ) (h : b > 0) : ∃ q r : ℕ, a = b * q + r ∧ r < b :=
 begin
